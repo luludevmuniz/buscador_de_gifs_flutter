99fbc1372cba3f6a281272f109aafc29a84151a8
luludevmuniz <lucasyavar@gmail.com>
2021-09-02 11:29:52 -0300

    Initial commit

---
+++ lib/ui/gif_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+class GifPage extends StatelessWidget {
+
+  final Map _gifData;
+
+   GifPage(this._gifData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_gifData["title"]),
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.share),
+              onPressed: (){
+            Share.share(_gifData["images"]["original"]["url"]);
+              })
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(_gifData["images"]["original"]["url"]), // fixed_height
+      ),
+    );
+  }
+}
